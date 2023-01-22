d8aea6780ebbe7c96d2d1d41d78c95f278623783
kimminkyeu <minkyeki@student.42seoul.kr>
2023-01-23 04:28:18 +0900

    :hammer: Initial set-up

---
+++ include/set.hpp
@@ -0,0 +1,8 @@
+//
+// Created by 김민규 on 2023/01/23.
+//
+
+#ifndef FT_CONTAINER_SET_HPP
+#define FT_CONTAINER_SET_HPP
+
+#endif //FT_CONTAINER_SET_HPP
--- test/main.cpp
+++ test/main.cpp
@@ -13,25 +13,68 @@
 // #include <functional>
 
 // #define TEST FT::_PRIVATE
-#define TEST FT
+#define TEST FT::_PRIVATE
 
-int		main(void)
+int		main()
 {
 	TEST::LeftLeaningRedBlack<int> test;
 
 	test.put(2);
+    TEST::printTree(test);
 	test.put(3);
+    TEST::printTree(test);
 	test.put(10);
+    TEST::printTree(test);
 	test.put(8);
+    TEST::printTree(test);
 	test.put(1);
+    TEST::printTree(test);
 	test.put(12);
+    TEST::printTree(test);
+    test.put(5);
+    TEST::printTree(test);
+    test.put(9);
+    TEST::printTree(test);
+    test.put(27);
+    TEST::printTree(test);
+    test.put(4);
+
 	TEST::printTree(test);
+    TEST::LeftLeaningRedBlack<int>::node_pointer ptr = test.begin();
+    while (ptr != test.end())
+    {
+        std::cout << ptr->key << std::endl;
+        ptr = ptr->getSuccessor();
+    }
+
+    std::cout << "\n--------------------\n";
+    std::cout << "Testing erase... \n";
+    std::cout << "--------------------\n";
+
 	test.erase(3);
 	TEST::printTree(test);
 	test.erase(10);
 	TEST::printTree(test);
 	test.erase(12);
 	TEST::printTree(test);
+    test.erase(8);
+    TEST::printTree(test);
+
+    ptr = test.begin();
+    while (ptr != test.end())
+    {
+        std::cout << ptr->key << std::endl;
+        ptr = ptr->getSuccessor();
+    }
+
+//    ptr = test.begin();
+//    while (ptr != test.end())
+//    {
+//        std::cout << ptr->key << std::endl;
+//        ptr = ptr->getSuccessor();
+//    }
+
+
 
 	return (0);
 }
